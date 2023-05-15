8d5b38af56e4a57956bf9d5e98f416c9039ebc3d
Rafael <rafael.kneip@engenharia.ufjf.br>
2023-05-15 10:23:30 -0300

    Commit

--- cancelados_fluig.py
+++ cancelados_fluig.py
@@ -1,33 +1,46 @@
+#Módulo para ler o site e obter os status das solicitações / Module to scrap the site and get the solicitations status
+
+#Importações / Imports
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import selenium.webdriver.support.expected_conditions as EC
 from time import sleep
 
+#Definir função / Define function
 def scraper(navegador, inicio, fluigs):
 
-  sleep(20)
-
+  #Definir listas auxiliares / Define auxiliary lists
   situacao=[]
   situacao_aux=[]
 
   for j in range(inicio,inicio+50): 
 
-    sleep(5)
+    #Garantir que a página está totalmente carregada / Make sure that the page is completely loaded
+    sleep(20)
 
-    print(fluigs[j][0])
+    print("Solcitação lida: ", fluigs[j][0])
 
+    #Acessar a página da solciitação / Acess the solicition page
     navegador.get("http://fluig.raizeducacao.com.br/portal/p/01/pageworkflowview?app_ecm_workflowview_detailsProcessInstanceID=" + str(fluigs[j][0]))
-
+    #Garantir que a seção da página está totalmente carregada / Make sure that the page section is completely loaded
     WebDriverWait(navegador,20).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="workflowView-cardViewer"]')))
+    #Selecionar elemento com as informações das solicitações / Get the element with the solicitations information
     site_lancamento = navegador.find_element(By.XPATH, '//*[@id="workflowView-cardViewer"]')
     navegador.switch_to.frame(site_lancamento)
 
+    #Definir listas auxiliares / Define auxiliary lists
     cancelado_str =[]
     escondidos_str =[]
 
+    #Cada página possui elementos para cada um dos status. O status real é definido pelo elemento que não é da class 'hide'/
+    #A element for each solicitation status can be find on every page. The real solicitation status is defined by the element that does not have the 'hide' class
+
+    #Obter o elemento que representa o status 'Cancelado' / Get the element with the 'Cancelled' status
     cancelado = (navegador.find_elements(By.CLASS_NAME, 'ativ-cancelado'))
+    #Obter os elementos com class 'hide' / Get the elements with 'hide' class 
     escondidos = (navegador.find_elements(By.CLASS_NAME, 'hide'))
 
+    #Converter variáveis selenium.webdriver.remote.webelement.WebElement para string / Convert 'selenium.webdriver.remote.webelement.WebElement' variables to string
     for i in range(len(cancelado)):
       cancelado_str.append(str(cancelado[i]))
 
@@ -36,6 +49,7 @@ def scraper(navegador, inicio, fluigs):
 
     contagem = (escondidos_str.count(cancelado_str[0]))
 
+    #Se o elemento 'cancelado' não está escondido na página, o chamada está, de fato, cancelado / If the 'canceled' element is not hided on the page, the solicitations is, in fact, cancelled
     if (contagem==0):
       situacao_aux.append('CANCELADO')
 
@@ -45,4 +59,5 @@ def scraper(navegador, inicio, fluigs):
     situacao.append(situacao_aux)
     situacao_aux = []
 
+  #Retornar lista / Return list
   return situacao
